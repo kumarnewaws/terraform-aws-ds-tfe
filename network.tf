83804ba422edb4faa5ed8c983496ad5648fe82c0
chaitanya <chaitanya@gmail.com>
2026-01-22 17:00:32 +0530

    deleted private subnet 4

--- network.tf
+++ network.tf
@@ -5,7 +5,7 @@ module "network" {
   vpc_name          = "Dev"
   PublicSubnetCidr  = ["10.20.1.0/24", "10.20.2.0/24", "10.20.3.0/24"]
   az                = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-  PrivateSubnetCidr = ["10.20.10.0/24", "10.20.20.0/24", "10.20.30.0/24","10.20.40.0/24"]
+  PrivateSubnetCidr = ["10.20.10.0/24", "10.20.20.0/24", "10.20.30.0/24"]
   env               = "dev"
   NGW_ID            = null
 }
